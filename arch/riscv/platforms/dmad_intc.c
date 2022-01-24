fa35fc60b85f465689199c5eea654d0267088a90
Yu Chien Peter Lin <peterlin@andestech.com>
2022-01-24 20:57:06 +0800

    riscv: DMA add msb bit patch
    Reformed from the commit:
    c32ef675cffe7a609d7afe2eb1ae92981a503144
    
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- arch/riscv/platforms/dmad_intc.c
+++ arch/riscv/platforms/dmad_intc.c
@@ -5,6 +5,9 @@
  */
 
 #include <linux/irq.h>
+#include <linux/irqchip.h>
+#include <linux/irqchip/chained_irq.h>
+#include <linux/irqdomain.h>
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
 #include <asm/io.h>
@@ -12,7 +15,49 @@
 
 #ifdef CONFIG_PLATFORM_AHBDMA
 extern int dmad_probe_irq_source_ahb(void);
-void AHBDMA_irq_rounter(struct irq_desc *desc)
+
+/*
+ * Generic dummy implementation which can be used for
+ * real dumb interrupt sources
+ */
+struct irq_chip atcdmac_irq_chip = {
+	.name		= "Andes DMAC",
+};
+
+struct ftdmac020_info {
+	int			parent_irq;
+	struct irq_domain	*irq_domain;
+};
+
+struct ftdmac020_info *ftdmac020;
+
+static int ftdmac020_irq_map(struct irq_domain *domain, unsigned int virq,
+			       irq_hw_number_t hwirq)
+{
+	irq_set_chip_and_handler(virq, &atcdmac_irq_chip, handle_simple_irq);
+	irq_set_chip_data(virq, domain->host_data);
+
+	return 0;
+}
+
+static void ftdmac020_irq_unmap(struct irq_domain *d, unsigned int virq)
+{
+	irq_set_chip_and_handler(virq, NULL, NULL);
+	irq_set_chip_data(virq, NULL);
+}
+
+static const struct irq_domain_ops ftdmac020_irq_ops = {
+	.map    = ftdmac020_irq_map,
+	.unmap  = ftdmac020_irq_unmap,
+};
+
+
+/*
+ * The atcdmac300 provides a single hardware interrupt for all of the dmad
+ * channel, so we use a self-defined interrupt chip to translate this single interrupt
+ * into multiple interrupts, each associated with a single dma channel.
+ */
+static void AHBDMA_irq_rounter(struct irq_desc *desc)
 {
 	int ahb_irq;
 	struct irq_desc *ahb_desc;
@@ -29,21 +74,47 @@ void AHBDMA_irq_rounter(struct irq_desc *desc)
 		raw_spin_lock(&desc->lock);
 	}
 	desc->irq_data.chip->irq_unmask(&desc->irq_data);
+	desc->irq_data.chip->irq_eoi(&desc->irq_data);
 	raw_spin_unlock(&desc->lock);
 }
 
-int intc_ftdmac020_init_irq(int irq)
+int ftdmac020_find_irq(int hwirq){
+	int virq;
+
+	virq = irq_find_mapping(ftdmac020->irq_domain, hwirq);
+	printk("[ftdmac020_irq_mapping]: virq=%d, hwirq=%d,\n",virq,hwirq);
+	if (!virq)
+		return -EINVAL;
+
+	return virq;
+}
+
+int ftdmac020_init(struct device_node *node, int irq)
 {
-	int i;
-	int ret;
-	/* Register all IRQ */
-	for (i = DMA_IRQ0;
-	     i < DMA_IRQ0 + DMA_IRQ_COUNT; i++) {
-		// level trigger
-		ret = irq_set_chip(i, &dummy_irq_chip);
-		irq_set_handler(i, handle_simple_irq);
+	int ret=0;
+
+	ftdmac020 = kzalloc(sizeof(struct ftdmac020_info), GFP_KERNEL);
+
+	ftdmac020->parent_irq=irq;
+
+	ftdmac020->irq_domain = __irq_domain_add(of_node_to_fwnode(node), DMA_IRQ_COUNT, DMA_IRQ0+DMA_IRQ_COUNT,
+					 ~0, &ftdmac020_irq_ops, ftdmac020);
+	if (!ftdmac020->irq_domain) {
+		printk("ftdmac020: Failed to create irqdomain\n");
+		return -EINVAL;
 	}
-	irq_set_chained_handler(irq, AHBDMA_irq_rounter);
+
+	ret = irq_create_strict_mappings(ftdmac020->irq_domain, DMA_IRQ0, DMA_IRQ0, DMA_IRQ_COUNT);
+	if(unlikely(ret < 0)){
+		printk("ftdmac020: Failed to create irq_create_strict_mappings()\n");
+		return -EINVAL;
+	}
+
+	ftdmac020->irq_domain->name = "ftdmac020-domain";
+	irq_set_chained_handler_and_data(ftdmac020->parent_irq,
+					 AHBDMA_irq_rounter, ftdmac020);
+
 	return 0;
 }
+
 #endif /* CONFIG_PLATFORM_AHBDMA */
